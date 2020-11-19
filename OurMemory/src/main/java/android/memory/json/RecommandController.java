110b320d02382c973cb89432340ea6101c186a6e
InjaeLee-new <eij9404@naver.com>
2020-11-19 13:14:02 +0900

    .

--- OurMemory/src/main/java/android/memory/json/RecommandController.java
+++ OurMemory/src/main/java/android/memory/json/RecommandController.java
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
