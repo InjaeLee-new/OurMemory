59556c291f6959ea62fc797e92030fe52f7b93fd
seung_won <71866565+aoqnwnd@users.noreply.github.com>
2020-11-19 12:46:20 +0900

    Update RecommandController.java
    어플 안에서 추천 / 비추천 기능.

--- OurMemory/src/main/java/android/memory/json/RecommandController.java
+++ OurMemory/src/main/java/android/memory/json/RecommandController.java
@@ -1,5 +1,66 @@
 package android.memory.json;
 
-public class RecommandController {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import OurMemory.service.OurMemoryService;
+
+@Controller
+public class RecommandController {
+	@Autowired
+	OurMemoryService ourMemoryService;
+	
+	// Android에서 추천에 관한 Controller
+	@RequestMapping(value = "/recommendation")
+	public void recommendation
+	(HttpServletRequest request, HttpServletResponse response) {
+	
+		int memory_num = Integer.parseInt(request.getParameter("memory_num"));
+		
+		int result = ourMemoryService.memoryBoardRec(memory_num);
+		String rt = "Fail";
+	    if(result > 0) rt = "OK";
+		
+		JSONObject json = new JSONObject();
+		json.put("rt",rt);
+		try {
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	// Android에서 비추천에 관한 Controller
+	@RequestMapping(value = "/notrecommendation")
+	public void	notrecommendation
+	(HttpServletRequest request, HttpServletResponse response) {
+		
+		int memory_num = Integer.parseInt(request.getParameter("memory_num"));
+		
+		int result = ourMemoryService.memoryBoardNrec(memory_num);
+		
+		String rt = "Fail";
+	    if(result > 0) rt = "OK";
+		
+		JSONObject json = new JSONObject();
+		json.put("rt",rt);
+		try {
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
