2098f62fc6b3a6d72d149d65aedb9b11280d6f5c
mysoung00 <71871624+mysoung00@users.noreply.github.com>
2020-11-27 17:38:17 +0900

    글 삭제 DeleteJson 추가
    글삭제용 DeleteJson 추가

--- OurMemory/src/main/java/android/memory/json/DeleteJson.java
+++ OurMemory/src/main/java/android/memory/json/DeleteJson.java
@@ -1,5 +1,47 @@
 package android.memory.json;
 
-public class DeleteJson {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import OurMemory.service.OurMemoryService;
+
+@Controller
+public class DeleteJson {
+	@Autowired
+	OurMemoryService ourMemoryService;
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/DeleteJson")
+	public void memberDelete(HttpServletRequest request, HttpServletResponse response) {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		int result = 0;
+		String rt = null;
+		
+		int memory_num = Integer.parseInt(request.getParameter("memory_num"));
+		result = ourMemoryService.memoryDelete(memory_num);
+		
+		if(result > 0) {
+			rt = "성공!";
+			JSONObject json = new JSONObject();
+			json.put("rt", rt);
+			
+			PrintWriter out;
+			try {
+				out = response.getWriter();
+				out.println(json);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
