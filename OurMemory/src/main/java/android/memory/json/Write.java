00acddebcc43cd16d6d67084894a0d845ead25b7
seung_won <71866565+aoqnwnd@users.noreply.github.com>
2020-11-27 16:01:30 +0900

    Update Write.java
    승원) 파일 여러개 올리기 기능 구현

--- OurMemory/src/main/java/android/memory/json/Write.java
+++ OurMemory/src/main/java/android/memory/json/Write.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletResponse;
@@ -24,50 +25,49 @@ import memory.dto.MemoryDTO;
 public class Write {
 	@Autowired
 	OurMemoryService ourMemoryService;
-	//l 찾았다 zzzzㅋㅋㅋ걍 복붙해놨었네 맞다
+	
 	@RequestMapping(value = "/writeAndroid")
 	public void memoryWrite(MultipartHttpServletRequest request, HttpServletResponse response) throws Exception {
-		String dir = "D:\\android_Kim_Seungwon\\spring\\workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\OurMemory1107\\storage";
+		
+		// 승원 집 컴퓨터 경로
+		String dir = "D:\\android_Kim_Seungwon\\spring\\workspace\\.metadata\\.plugins\\org.eclipse.wst.server.core\\tmp0\\wtpwebapps\\OurMemory1107\\storage\\";
 
 		String filePath = "C:\\Users\\USER\\git\\repository\\OurMemory\\src\\main\\webapp\\img";
-		MultipartFile img = request.getFile(("memory_file"));
-		System.out.println("img : "+img);
+		// 파일 여러개 올리게 리스트 형식으로 작성				files 's' 확인
+		List<MultipartFile> imgList = request.getFiles("memory_file");
+		System.out.println("img : "+imgList);
 //		
 //		String fileName = img.getOriginalFilename();
 //		
 //		File file = new File(dir, fileName);
-		String originname = img.getOriginalFilename();
-		UUID uuid = UUID.randomUUID(); // 랜덤 글 생성
-		String filename = uuid+originname; //파일이름 중복방지
-		int lastIndex = originname.lastIndexOf("."); // 오른쪽에서 왼쪽으로 가면서 마지막 글을 찾는 것
-		String filetype = originname.substring(lastIndex +1);
-		img.transferTo(new File(dir+filename)); //파일로 저장하기
+		String arrayFileName="";
 		
-		// 파일 복사
-		// getInputStream() : 업로드한 파일 데이터를 읽어오는 InputStream을 구한다.gg
-//		try {
-//			FileCopyUtils.copy(img.getInputStream(), new FileOutputStream(file));
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		for (int i = 0; i< imgList.size(); i++) {
+			MultipartFile mf = imgList.get(i);
+			String originname = mf.getOriginalFilename();
+//			일단 주석처리 한 곳은 파일 이름 동일시 안되게 하려고 한것임
+//			UUID uuid = UUID.randomUUID(); // 랜덤 글 생성  
+//			String filename = uuid+originname; //파일이름 중복방지
+			int lastIndex = originname.lastIndexOf("."); // 온전히 파일 이름만 저장
+			String filetype = originname.substring(lastIndex +1); // 파일타입 구분 (jpg, png, gif)
+			mf.transferTo(new File(dir+originname)); //파일로 저장하기
+			if (i==imgList.size()-1) {
+				arrayFileName += originname;
+			}else {
+				arrayFileName += originname+", ";				
+			}
+		}
+		
 		
 		MemoryDTO memoryDto = new MemoryDTO();
-		memoryDto.setMemory_name(request.getParameter("memory_name"));
+		
 		System.out.println("memory_name : "+request.getParameter("memory_name"));
-		memoryDto.setMemory_pass(request.getParameter("memory_pass"));
 		System.out.println("memory_pass : "+request.getParameter("memory_pass"));
-		memoryDto.setMemory_id(request.getParameter("memory_id"));
 		System.out.println("memory_id : "+request.getParameter("memory_id"));
-		memoryDto.setMemory_subject(request.getParameter("memory_subject"));
 		System.out.println("memory_subject : "+request.getParameter("memory_subject"));
-		memoryDto.setMemory_content(request.getParameter("memory_content"));
 		System.out.println("memory_content : "+request.getParameter("memory_content"));
-		memoryDto.setMemory_category(request.getParameter("memory_category"));
 		System.out.println("memory_category : "+request.getParameter("memory_category"));
-		memoryDto.setMemory_file(originname);
-		System.out.println("originname : "+originname);
+		System.out.println("originname : "+arrayFileName);
 		
 		int result = ourMemoryService.writeAndroid(memoryDto);
 		String rt = "Fail";
