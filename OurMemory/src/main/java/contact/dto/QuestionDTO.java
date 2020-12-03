d17114ae2d2d99ac3ac266744dd520075a1c152e
seung_won <71866565+aoqnwnd@users.noreply.github.com>
2020-12-03 15:31:12 +0900

    Update QuestionDTO.java
    승원) DTO 추가

--- OurMemory/src/main/java/contact/dto/QuestionDTO.java
+++ OurMemory/src/main/java/contact/dto/QuestionDTO.java
@@ -1,5 +1,34 @@
 package contact.dto;
 
 public class QuestionDTO {
-
+	private int question_num;
+	private String question_subject;
+	private String question_content;
+	private String question_logtime;
+	public int getQuestion_num() {
+		return question_num;
+	}
+	public void setQuestion_num(int question_num) {
+		this.question_num = question_num;
+	}
+	public String getQuestion_subject() {
+		return question_subject;
+	}
+	public void setQuestion_subject(String question_subject) {
+		this.question_subject = question_subject;
+	}
+	public String getQuestion_content() {
+		return question_content;
+	}
+	public void setQuestion_content(String question_content) {
+		this.question_content = question_content;
+	}
+	public String getQuestion_logtime() {
+		return question_logtime;
+	}
+	public void setQuestion_logtime(String question_logtime) {
+		this.question_logtime = question_logtime;
+	}
+	
+	
 }
