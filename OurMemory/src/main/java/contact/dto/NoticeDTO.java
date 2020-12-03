b98fe486010e224f0a8f011f67c180fc223b0861
seung_won <71866565+aoqnwnd@users.noreply.github.com>
2020-12-03 15:31:30 +0900

    Update NoticeDTO.java
    승원) DTO 추가

--- OurMemory/src/main/java/contact/dto/NoticeDTO.java
+++ OurMemory/src/main/java/contact/dto/NoticeDTO.java
@@ -1,5 +1,33 @@
 package contact.dto;
 
 public class NoticeDTO {
-
+	private int notice_num;
+	private String notice_subject;
+	private String notice_content;
+	private String notice_logtime;
+	public int getNotice_num() {
+		return notice_num;
+	}
+	public void setNotice_num(int notice_num) {
+		this.notice_num = notice_num;
+	}
+	public String getNotice_subject() {
+		return notice_subject;
+	}
+	public void setNotice_subject(String notice_subject) {
+		this.notice_subject = notice_subject;
+	}
+	public String getNotice_content() {
+		return notice_content;
+	}
+	public void setNotice_content(String notice_content) {
+		this.notice_content = notice_content;
+	}
+	public String getNotice_logtime() {
+		return notice_logtime;
+	}
+	public void setNotice_logtime(String notice_logtime) {
+		this.notice_logtime = notice_logtime;
+	}
+	
 }
