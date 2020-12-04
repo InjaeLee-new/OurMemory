9f9a3c830e7d8ffd157277b9b93f057ad41a974d
seung_won <71866565+aoqnwnd@users.noreply.github.com>
2020-12-04 15:08:23 +0900

    Update ArtListJson.java
    승원) 리스트 출력 수정

--- OurMemory/src/main/java/android/memory/json/ArtListJson.java
+++ OurMemory/src/main/java/android/memory/json/ArtListJson.java
@@ -1,55 +1,71 @@
-//package android.memory.json;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.json.simple.JSONObject;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import OurMemory.service.OurMemoryService;
-//import recommand.dto.RecommandDTO;
-//
-//@Controller
-//public class ArtListJson {
-//	@Autowired
-//	OurMemoryService ourMemoryService;
-//	
-//	// Android에서 추천 중복 불가에 대한 코드
-//	@RequestMapping(value = "/recommandCheck")
-//	public void recommendation
-//	(HttpServletRequest request, HttpServletResponse response) {
-//	
-//		int recommand_seq = Integer.parseInt(request.getParameter("recommand_seq"));
-//		String recommand_id = request.getParameter("recommand_id");
-//		System.out.println(recommand_id+" = recommand_id");
-//		int result = ourMemoryService.recommandCheck(recommand_seq, recommand_id);
-//		System.out.println(result+" = result");
-//		
-//		String rt = "Exist"; 
-//	    if(result == 0) {
-//	    	RecommandDTO dto = new RecommandDTO();
-//	    	dto.setRecommand_id(recommand_id);
-//	    	dto.setRecommand_seq(recommand_seq);
-//	    	int result2 = ourMemoryService.recommandInsert(dto);
-//	    	System.out.println(result2+" = result2");
-//	    	if (result2 > 0) {
-//	    		rt = "OK";	    		
-//	    	}
-//	    }
-//		JSONObject json = new JSONObject();
-//		json.put("rt",rt);
-//		try {
-//			response.setCharacterEncoding("UTF-8");
-//			response.setContentType("text/html;charset=UTF-8");
-//			PrintWriter out = response.getWriter();
-//			out.println(json);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
+package android.memory.json;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import OurMemory.service.OurMemoryService;
+import memory.dto.MemoryDTO;
+
+@Controller
+public class ArtListJson {
+	@Autowired
+	OurMemoryService ourMemoryService;
+	
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/artListJson")
+	public void listJson(HttpServletRequest request, HttpServletResponse response) {
+		
+		List<MemoryDTO> list = ourMemoryService.boardList("art");
+		
+		String rt = null;
+		if(list != null) {
+		   
+		   rt = "성공!!";
+		   JSONObject json = new JSONObject();
+		   json.put("rt", rt);
+		   json.put("total", list.size());
+		   
+		   JSONArray memoryList = new JSONArray();
+		   
+		   for(int i = 0 ; i < list.size() ; i++) {
+			    JSONObject select = new JSONObject();
+		        select.put("memory_num", list.get(i).getMemory_num());
+		        select.put("memory_file", list.get(i).getMemory_file());
+		        select.put("memory_subject", list.get(i).getMemory_subject());
+		        select.put("memory_content", list.get(i).getMemory_content());
+		        select.put("memory_date", list.get(i).getMemory_date());
+		        select.put("memory_rec", list.get(i).getMemory_rec());
+		        select.put("memory_hit", list.get(i).getMemory_hit());
+		        select.put("memory_nrec", list.get(i).getMemory_nrec());
+		        select.put("memory_name", list.get(i).getMemory_name());
+		        select.put("memory_category", list.get(i).getMemory_category());
+		        
+		        memoryList.add(select);
+		   }
+		   
+		   json.put("memoryList", memoryList);
+		   
+		try {
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		  
+		}
+		
+	}
+}
