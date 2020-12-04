cddcfe3c5a3ebc057d852670a2feface43912229
mysoung00 <71871624+mysoung00@users.noreply.github.com>
2020-12-04 15:02:02 +0900

    MapDAO (성빈
    MapDAO 추가하기 Insert, List(성빈

--- OurMemory/src/main/java/map/dao/MapDAO.java
+++ OurMemory/src/main/java/map/dao/MapDAO.java
@@ -1,5 +1,38 @@
 package map.dao;
 
-public class MapDAO {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import contact.dto.ContactDTO;
+import map.dto.MapDTO;
+
+
+@Repository
+public class MapDAO {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	//insert : 글저장
+	public int mapInsert(MapDTO mapDTO) {
+		int result = sqlSession.insert("mybatis.mapMapper.mapInsert", mapDTO);
+		return result;
+	}
+		
+	//select : 목록 List 불러오기
+	public List<MapDTO> mapList(MapDTO mapDTO) {
+		List<MapDTO> list = sqlSession.selectList("mybatis.mapMapper.mapList", mapDTO);
+		return list;
+	}	
 }
+
+
+
+
+
+
