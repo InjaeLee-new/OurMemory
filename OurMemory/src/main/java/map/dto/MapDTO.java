8494da07047e14ff71328101ada5e924797f76a9
mysoung00 <71871624+mysoung00@users.noreply.github.com>
2020-12-04 15:02:48 +0900

    MapDTO 추가 (성빈
    MapDTO 추가하기 address

--- OurMemory/src/main/java/map/dto/MapDTO.java
+++ OurMemory/src/main/java/map/dto/MapDTO.java
@@ -2,4 +2,12 @@ package map.dto;
 
 public class MapDTO {
 
+	private String address;		//주소
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	} 
 }
